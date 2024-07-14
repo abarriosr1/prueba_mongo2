adf19eb12096da05133df5ec140824bbadea6cc4
safimmi <asztangie2@gmail.com>
2024-07-14 17:38:42 -0500

    chore : set dotenv as a dev dependecy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 
 const database = require('./config/database');
