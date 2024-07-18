5566dfd31516840a3eeace7cdbae49f5451b1144
safimmi <asztangie2@gmail.com>
2024-07-18 08:46:11 -0500

    fix : remove whitespace before checking if predial id is falsy

--- server/middlewares/validatePredialId.js
+++ server/middlewares/validatePredialId.js
@@ -4,13 +4,14 @@ const validatePredialId = (req, res, next) => {
   let { id } = req.params;
   const isPublic = res.locals.public ? res.locals.public : false;
 
+  id = removeAllWhitespace(id);
+  req.params.id = id;
+
   if (!id) {
     const error = isPublic ? "Se necesita un número de ficha" : "Predial id is needed";
     res.status(400);
     return next(new Error(error));
   }
-
-  id = removeAllWhitespace(id);
 
   if (isNaN(id)) {
     const error = isPublic ? "El número de ficha debe ser numérico" : "Predial id must be numeric";
@@ -18,7 +19,6 @@ const validatePredialId = (req, res, next) => {
     return next(new Error(error));
   }
 
-  req.params.id = id;
   next();
 };
 
